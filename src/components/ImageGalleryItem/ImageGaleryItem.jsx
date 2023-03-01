b6dcbbcf1a7ac1a4bc182e065db7f9184af624f8
GROM-85 <richfron69@gmail.com>
2023-03-01 23:24:40 +0100

    dep1

--- src/components/ImageGalleryItem/ImageGaleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGaleryItem.jsx
@@ -1,18 +1,17 @@
 import css from './ImageGalleryItem.module.scss';
 import PropTypes from 'prop-types';
 
-export const ImageGalleryItem = ({img,largeImg,tags,showModal}) => {
+export const ImageGalleryItem = ({ img, largeImg, tags, showModal }) => {
   return (
     <li className={css.gallery__item}>
-      <img src={img} alt={tags} onClick={() => showModal(largeImg)}/>
+      <img src={img} alt={tags} onClick={() => showModal(largeImg)} />
     </li>
   );
 };
 
-
 ImageGalleryItem.propTypes = {
-  img:PropTypes.string.isRequired,
-  largeImg:PropTypes.string.isRequired,
-  tags:PropTypes.string.isRequired,
-  showModal:PropTypes.func.isRequired,
-}
+  img: PropTypes.string.isRequired,
+  largeImg: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  showModal: PropTypes.func.isRequired,
+};
