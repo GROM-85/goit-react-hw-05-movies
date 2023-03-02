396bf5c3266186acc40ee5e73a01ab4c636f0fff
GROM-85 <richfron69@gmail.com>
2023-03-02 10:29:43 +0100

    dep2

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
 import css from './ImageGallery.module.scss';
@@ -7,79 +7,79 @@ import { Button } from 'components/Button';
 import { Loader } from 'components/Loader/Loader';
 import Notiflix from 'notiflix';
 
+export const ImageGallery = ({ query, showModal }) => {
+  const [hits, setHits] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-export const ImageGallery = ({query,showModal}) =>{
- const[hits,setHits] = useState([]);
- const [page,setPage] = useState(1);
- const [isLoading,setIsLoading] = useState(false);
+  const resetToDefaults = () => {
+    setPage(1);
+    setHits([]);
+  };
 
- const resetToDefaults = () => { 
-  setPage(1);
-  setHits([]);
-}
+  const updatePage = () => {
+    setPage(state => state + 1);
+  };
 
-const updatePage = () =>{
-  setPage(state => state + 1)
-}
+  const updateData = async () => {
+    setIsLoading(true);
+    try {
+      const data = await getData({ query, page });
+      if (data.hits.length === 0)
+        Notiflix.Notify.info('Nothing has been found!');
+      setHits([...hits, ...data.hits]);
+    } catch (error) {
+      console.log(error?.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-const updateData = async() =>{
-  setIsLoading(true);
-  try {
-    const data = await getData({ query, page });
-    if(data.hits.length === 0) Notiflix.Notify.info("Nothing has been found!")
-    setHits([...hits, ...data.hits]);
-  } catch (error) {
-    console.log(error?.message);
-  } finally {
-    setIsLoading(false);
-  }
-}
-
- useEffect(() => {
-  if(!query) return;
+  useEffect(() => {
+    if (!query) return;
     resetToDefaults();
     updateData();
     // eslint-disable-next-line
- },[query])
+  }, [query]);
 
- useEffect(() => {
-  if(page === 1) return;
-  updateData();
-  // eslint-disable-next-line
- },[page])
+  useEffect(() => {
+    if (page === 1) return;
+    updateData();
+    // eslint-disable-next-line
+  }, [page]);
 
- return (
-  <>
-    {hits.length !== 0 && (
-      <>
-        <ul className={css.gallery}>
-          {hits.map(({ id, webformatURL, tags , largeImageURL}) => (
-            <ImageGalleryItem
-              key={id}
-              img={webformatURL}
-              tags={tags}
-              showModal={showModal}
-              largeImg={largeImageURL}
+  return (
+    <>
+      {hits.length !== 0 && (
+        <>
+          <ul className={css.gallery}>
+            {hits.map(({ id, webformatURL, tags, largeImageURL }) => (
+              <ImageGalleryItem
+                key={id}
+                img={webformatURL}
+                tags={tags}
+                showModal={showModal}
+                largeImg={largeImageURL}
+              />
+            ))}
+          </ul>
+          {!isLoading ? (
+            <Button
+              title="Load More"
+              onClick={updatePage}
+              type="button"
+              className={css.load__more}
             />
-          ))}
-        </ul>
-        {!isLoading ? (
-          <Button
-            title="Load More"
-            onClick={updatePage}
-            type="button"
-            className={css.load__more}
-          />
-        ) : (
-          <Loader />
-        )}
-      </>
-    )}
-  </>
-)
-}
+          ) : (
+            <Loader />
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
 ImageGallery.propTypes = {
-  query:PropTypes.string.isRequired,
-  showModal:PropTypes.func.isRequired,
-}
+  query: PropTypes.string.isRequired,
+  showModal: PropTypes.func.isRequired,
+};
